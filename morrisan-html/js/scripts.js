b5c664432e3bc91cf079d359b2faf961c1407098
marbekerasyl <107627807+marbekerasyl@users.noreply.github.com>
2022-06-16 19:12:54 +0600

    my work

---
+++ morrisan-html/js/scripts.js
@@ -0,0 +1,312 @@
+$(document).ready(function () {
+  let hiddenRow = $(".madrid");
+  $("#content-btn").click(function () {
+      hiddenRow.toggleClass("real");
+      if (hiddenRow.is(":visible")) {
+          $("#content-btn").html("Decrease");
+      } else {
+          $("#content-btn").html("Expand");
+      }
+ });
+});
+$(document).ready(function(){
+  var div = $("#divToShowHide");
+  var pos = div.position();
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   if (windowpos >= (pos.top - 0)) {
+     div.addClass("AfterScroll");
+   }    
+    else {
+      div.removeClass("AfterScroll");
+    }
+ });
+});   
+
+$(document).ready(function(){
+var sox = $("#divShow");
+var sop = sox.position();
+$(window).scroll(function () {
+var pos = $(window).scrollTop();
+if (pos >= (sop.top - 400)) {
+sox.addClass("After");
+}    
+else {
+sox.removeClass("After");
+}
+});
+});
+$(document).ready(function(){
+  var vid = $("#Hide");
+  var xos = vid.position();
+  $(window).scroll(function () {
+   var dow = $(window).scrollTop();
+   if (dow >= (xos.top - 0)) {
+     vid.addClass("roll");
+   }    
+    else {
+      vid.removeClass("roll");
+    }
+ });
+}); 
+$(document).ready(function(){
+    var div = $("#div");
+    var pos = div.position();
+    $(window).scroll(function () {
+     var windowpos = $(window).scrollTop();
+     if (windowpos >= (pos.top - 0)) {
+       div.addClass("Af");
+     }    
+      else {
+        div.removeClass("Af");
+      }
+   });
+  });
+$(document).ready(function(){
+      var div = $("#cool");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 0)) {
+         div.addClass("fit");
+       }    
+        else {
+          div.removeClass("fit");
+        }
+     });
+    });      
+$(document).ready(function(){
+      var div = $("#inner");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 0)) {
+         div.addClass("four");
+       }    
+        else {
+          div.removeClass("four");
+        }
+     });
+    });
+$(document).ready(function(){
+      var div = $("#five");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 400)) {
+         div.addClass("seven");
+       }    
+        else {
+          div.removeClass("seven");
+        }
+     });
+    }); 
+$(document).ready(function(){
+      var div = $("#eight");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 400)) {
+         div.addClass("ten");
+       }    
+        else {
+          div.removeClass("ten");
+        }
+     });
+    }); 
+$(document).ready(function(){
+      var div = $("#eleven");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 0)) {
+         div.addClass("thirteen");
+       }    
+        else {
+          div.removeClass("thirteen");
+        }
+     });
+    });
+$(document).ready(function(){
+      var div = $("#fourteen");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 0)) {
+         div.addClass("sixteen");
+       }    
+        else {
+          div.removeClass("sixteen");
+        }
+     });
+    });
+$(document).ready(function(){
+      var div = $("#seventeen");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 400)) {
+         div.addClass("nineteen");
+       }    
+        else {
+          div.removeClass("nineteen");
+        }
+     });
+    });
+$(document).ready(function(){
+      var div = $("#twenty");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 400)) {
+         div.addClass("twentytwo");
+       }    
+        else {
+          div.removeClass("twentytwo");
+        }
+     });
+    });
+$(document).ready(function(){
+      var div = $("#twentythree");
+      var pos = div.position();
+      $(window).scroll(function () {
+       var windowpos = $(window).scrollTop();
+       if (windowpos >= (pos.top - 400)) {
+         div.addClass("twentyfive");
+       }    
+        else {
+          div.removeClass("twentyfive");
+        }
+     });
+    });
+$(document).ready(function(){
+          var div = $("#twentyseven");
+            var pos = div.position();
+            $(window).scroll(function () {
+             var windowpos = $(window).scrollTop();
+             if (windowpos >= (pos.top - 0)) {
+               div.addClass("twentynine");
+             }    
+              else {
+                div.removeClass("twentynine");
+              }
+           });
+          });
+$(document).ready(function(){
+            var div = $("#thirty");
+            var pos = div.position();
+            $(window).scroll(function () {
+             var windowpos = $(window).scrollTop();
+             if (windowpos >= (pos.top - 0)) {
+               div.addClass("thirtytwo");
+             }    
+              else {
+                div.removeClass("thirtytwo");
+              }
+           });
+          });
+$(document).ready(function(){
+            var div = $("#thirtythree");
+            var pos = div.position();
+            $(window).scroll(function () {
+             var windowpos = $(window).scrollTop();
+             if (windowpos >= (pos.top - 0)) {
+               div.addClass("thirtyfive");
+             }    
+              else {
+                div.removeClass("thirtyfive");
+              }
+           });
+          });
+          
+$(document).ready(function(){
+  var div = $("#thirtysix");
+  var pos = div.position();
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   if (windowpos >= (pos.top - 0)) {
+     div.addClass("thirtyeight");
+   }    
+    else {
+      div.removeClass("thirtyeight");
+    }
+ });
+});
+$(document).ready(function(){
+  var div = $("#thirtynine");
+  var pos = div.position();
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   if (windowpos >= (pos.top - 0)) {
+     div.addClass("fortyone");
+   }    
+    else {
+      div.removeClass("fortyone");
+    }
+ });
+});
+$(document).ready(function(){
+  var div = $("#fortytwo");
+  var pos = div.position();
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   if (windowpos >= (pos.top - 0)) {
+     div.addClass("fortyfour");
+   }    
+    else {
+      div.removeClass("fortyfour");
+    }
+ });
+});
+$(document).ready(function(){
+  var div = $("#fortyfive");
+  var pos = div.position();
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   if (windowpos >= (pos.top - 0)) {
+     div.addClass("fortyseven");
+   }    
+    else {
+      div.removeClass("fortyseven");
+    }
+ });
+});
+$(document).ready(function(){
+  var div = $("#fortyeight");
+  var pos = div.position();
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   if (windowpos >= (pos.top - 0)) {
+     div.addClass("fivte");
+   }    
+    else {
+      div.removeClass("fivte");
+    }
+ });
+});
+$(document).ready(function(){
+  var div = $("#fivtyone");
+  var pos = div.position();
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   if (windowpos >= (pos.top - 0)) {
+     div.addClass("fivtythree");
+   }    
+    else {
+      div.removeClass("fivtythree");
+    }
+ });
+});
+$(document).ready(function(){
+  var div = $("#fivtythree");
+  var pos = div.position();
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   if (windowpos >= (pos.top - 0)) {
+     div.addClass("fivtyfive");
+   }    
+    else {
+      div.removeClass("fivtyfive");
+    }
+ });
+});
